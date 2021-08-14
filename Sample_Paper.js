8ffa029927c1ab45bad91fe822a35bf18802a0e8
Nishant Chaudhary <bohrabadal@gmail.com>
2021-08-14 14:22:47 +0530

    Initial

---
+++ Sample_Paper.js
@@ -0,0 +1,25 @@
+console.log("this is working");
+const APP_BASE_URL= "https://file-sharing-myfirst.herokuapp.com/";
+const paperurl=`${APP_BASE_URL}sendall`;
+
+
+
+fetch(paperurl).then(response => response.json())
+.then((data )=> 
+{
+    console.log(data);
+    for(key in data)
+    {
+        // console.log(data[key]);
+        // console.log('another one');
+        let table=document.querySelector('.table');
+        let link='${APP_BASE_URL}files/download/${data[key]}.uuid';
+        // console.log(link);
+        table.innerHTML+=`<tr>
+        <th scope="row">1</th>
+        <td>${data[key].filename}</td>
+        <td> <a href=${APP_BASE_URL}files/download/${data[key].uuid}>download</a></td>
+    </tr>`
+    }
+
+});
